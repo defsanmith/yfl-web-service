dd14119776f6d4cc561a795d7f8bf01e61687f40
Edwin Miyatake <edmiyatake@gmail.com>
2025-11-18 20:19:30 -0800

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,8 +2,6 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-
-  schema: "prisma/schema.prisma",
   migrations: {
     seed: "tsx prisma/seed.ts",
   },
